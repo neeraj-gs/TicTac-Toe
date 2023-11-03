76437d8691e273080807f71fe3bd65f514b13340
neeraj-gs <gsneeraj05@gmail.com>
2023-11-03 18:16:29 +0530

    Added a Winning Strike gui on winning the game

--- tic-tac-toe/src/components/TicTacToe.jsx
+++ tic-tac-toe/src/components/TicTacToe.jsx
@@ -1,13 +1,43 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Board from "./Board"
 
 const playerx = "X";
 const playero = "O";
 
+const winningCombo = [
+    //ROws
+    {combo:[0,1,2],strike:'strike-row-1'},
+    {combo:[3,4,5],strike:'strike-row-2'},
+    {combo:[6,7,8],strike:'strike-row-3'},
+
+    //Columns
+    {combo:[0,3,6],strike:'strike-column-1'},
+    {combo:[1,4,7],strike:'strike-column-2'},
+    {combo:[2,5,8],strike:'strike-column-3'},
+
+    //Diagonals
+    {combo:[0,4,8],strike:'strike-diagnol-1'},
+    {combo:[2,4,6],strike:'strike-diagnol-2'},
+]
+
+const checkWinner = (tiles,setStrike)=>{
+    for(const {combo,strike} of winningCombo ){
+        const tileValue1 = tiles[combo[0]]
+        const tileValue2 = tiles[combo[1]]
+        const tileValue3 = tiles[combo[2]]
+
+        if(tileValue1 !== null &&  tileValue1 === tileValue2 && tileValue1 === tileValue3){
+            setStrike(strike)
+            return true
+        }
+    }
+}
+
+
 const TicTacToe = () => {
     const [tiles,setTiles] = useState(Array(9).fill(null))
     const [playerTurn,setPlayerTurn] = useState(playerx)
-    const [strike,setStrike] = useState("strike-row-1");
+    const [strike,setStrike] = useState();
 
     const handleTileClick = (index)=>{
 
@@ -24,6 +54,10 @@ const TicTacToe = () => {
             setPlayerTurn(playerx)
         }
     }
+
+    useEffect(()=>{
+        checkWinner(tiles,setStrike);
+    },[tiles])
 
 
   return (
