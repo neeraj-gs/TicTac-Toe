ff4b99098bbed526e11f5dc62707875b0d9e4e05
neeraj-gs <gsneeraj05@gmail.com>
2023-11-03 18:30:33 +0530

    Fixed the big of game result not printing properly

--- tic-tac-toe/src/components/GameOver.jsx
+++ tic-tac-toe/src/components/GameOver.jsx
@@ -1,34 +1,18 @@
-import GameState from "./GameState"
+import GameState from "./GameState";
 
-const GameOver = ({gameState}) => {
-  switch(gameState){
+function GameOver({ gameState }) {
+  switch (gameState) {
     case GameState.inProgress:
-      return (
-        <></>
-      )
-    case GameState.playerxwins:
-      return (
-        <div className="game-over">
-            <h1>Player X Won the Game</h1>
-        </div>
-      )
-    case GameState.playerowins:
-      return (
-        <div className="game-over">
-            <h1>Player O Won the Game</h1>
-        </div>
-      )
-      case GameState.draw:
-      return (
-        <div className="game-over">
-            <h1>Its A Draw Match</h1>
-        </div>
-      )
-      default:
-        return (
-          <></>
-        )
+      return <></>;
+    case GameState.playerOWins:
+      return <div className="game-over">O Wins</div>;
+    case GameState.playerXWins:
+      return <div className="game-over">X Wins</div>;
+    case GameState.draw:
+      return <div className="game-over">Draw</div>;
+    default:
+      return <></>;
   }
 }
 
-export default GameOver
+export default GameOver;
--- tic-tac-toe/src/components/GameState.js
+++ tic-tac-toe/src/components/GameState.js
@@ -1,8 +1,8 @@
 const GameState = {
-    playerxwins:0,
-    playerowins:1,
-    draw:2,
-    inProgress:3,
-}
-
-export default GameState;
+    playerXWins: 0,
+    playerOWins: 1,
+    draw: 2,
+    inProgress: 3,
+  };
+  
+  export default GameState;
