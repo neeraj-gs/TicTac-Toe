dd1cf327f57df7a1b5cf28f9a3e84ae164331753
neeraj-gs <gsneeraj05@gmail.com>
2023-11-14 12:00:41 +0530

    Fixed teh strike type safety

--- tic-tac-toe-typescript/src/components/Strike.tsx
+++ tic-tac-toe-typescript/src/components/Strike.tsx
@@ -1,4 +1,8 @@
-const Strike = ({strike}) => {
+type StrikeProps = {
+  strike: string | undefined;
+};
+
+const Strike:React.FC<StrikeProps> = ({strike}) => {
   return (
     <div className={`strike ${strike}`}></div>
   )
